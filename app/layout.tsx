065a513c046ef7d5f0dc30fa2c6a4e43df7cc8bd
moistest22 <noahcsaxe22@gmail.com>
2025-02-22 23:45:57 -0500

    button style

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`h-full ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,8 @@
-import ContractAddress from "./components/ContractAddress";
-
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center p-4">
-      <div className="bg-white rounded-lg shadow-md p-6 max-w-2xl w-full">
-        <h1 className="text-2xl font-bold text-gray-800 mb-4">
-          Contract Address
-        </h1>
-        <ContractAddress />
-      </div>
+    <div className="bg-black border h-screen">
+      <h1 className="text-6xl">Contract Address</h1>
+      <button className="border bg-green-600">ygbbygbyg </button>
     </div>
   );
 }
